8ec32d2bf96c84268a74e309005dcd0e1d51ccfb
Saket Patil <128901142+Swollenjelly@users.noreply.github.com>
2025-02-17 18:05:45 +0530

    Add solution for 200. Number of Islands

---
+++ LeetCode/200._Number_of_Islands.py
@@ -0,0 +1,38 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        int numOfIslands = 0;
+
+        for(int i = 0; i< grid.length; i++){
+            for(int j = 0; j < grid[i].length; j++){
+                if(grid[i][j] == '1'){
+                    traverseIsland(grid,i,j);
+                    numOfIslands++;
+                }
+            }
+        }
+        return numOfIslands;
+    }
+
+    private void traverseIsland(char[][]grid, int i, int j){
+        if(grid[i][j]=='0'){
+            return;
+        }
+        grid[i][j]= '0';
+
+        if(i>0){
+            traverseIsland(grid, i-1,j);
+        }
+
+        if(i<grid.length-1){
+            traverseIsland(grid,i+1,j);
+        }
+
+        if(j>0){
+            traverseIsland(grid, i, j-1);
+        }
+
+        if(j<grid[0].length-1){
+            traverseIsland(grid,i,j+1);
+        }
+    }
+}
